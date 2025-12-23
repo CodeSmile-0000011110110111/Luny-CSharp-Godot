3dfe148bc7b5a17fca6d0a0d7d98bfbee2c5d7f4
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-23 18:38:12 +0100

    add Godot time service

---
+++ Providers/GodotTimeServiceProvider.cs
@@ -0,0 +1,17 @@
+﻿using Luny.Interfaces.Providers;
+using System;
+using Godot;
+
+namespace Luny.Godot.Providers
+{
+	/// <summary>
+	/// Godot implementation of time service provider.
+	/// </summary>
+	public sealed class GodotTimeServiceProvider : ITimeServiceProvider
+	{
+		// downcast isn't an issue: even at 10,000 fps it would take nearly 30 million years before it overflows!
+		public Int64 FrameCount => (long)Engine.GetProcessFrames();
+
+		public Double ElapsedSeconds => Time.GetTicksMsec() / 1000.0;
+	}
+}
