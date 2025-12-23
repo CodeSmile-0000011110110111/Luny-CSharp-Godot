8f005a0ffba693fa8369e84f1d8d797fb2306657
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-23 20:35:37 +0100

    fix compile error, update usings

--- Proxies/GodotLogger.cs
+++ Proxies/GodotLogger.cs
@@ -1,17 +1,17 @@
 using Godot;
-using Luny.Interfaces;
+using Luny.Diagnostics;
 using System;
 
 namespace Luny.Godot
 {
-    internal sealed class GodotLogger : ILunyLogger
-    {
-        public void LogInfo(String message) => GD.Print(message);
+	internal sealed class GodotLogger : ILunyLogger
+	{
+		public void LogInfo(String message) => GD.Print(message);
 
-        public void LogWarning(String message) => GD.PushWarning(message);
+		public void LogWarning(String message) => GD.PushWarning(message);
 
-        public void LogError(String message) => GD.PushError(message);
+		public void LogError(String message) => GD.PushError(message);
 
-        public void LogException(Exception exception) => GD.PushError(exception);
-    }
+		public void LogException(Exception exception) => GD.PushError(exception);
+	}
 }
